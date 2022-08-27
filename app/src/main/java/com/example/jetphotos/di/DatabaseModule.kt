d27f04b22848d96f3be46e7ac76057e760404cb7
Arfin Hosain <developerarfin@gmail.com>
2022-08-27 13:11:46 +0600

    Nothing Added

--- app/src/main/java/com/example/jetphotos/di/DatabaseModule.kt
+++ app/src/main/java/com/example/jetphotos/di/DatabaseModule.kt
@@ -1,10 +1,16 @@
 package com.example.jetphotos.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+
+    @Provides
+    fun SIngleOtn(){
+
+    }
 }
