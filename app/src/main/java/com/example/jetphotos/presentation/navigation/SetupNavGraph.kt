bd48dce6d89de4aa8cf6901eb75145305b11eb7b
Arfin Hosain <developerarfin@gmail.com>
2022-08-27 12:42:11 +0600

    Added a Log.d statement

--- app/src/main/java/com/example/jetphotos/presentation/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/example/jetphotos/presentation/navigation/SetupNavGraph.kt
@@ -1,5 +1,6 @@
 package com.example.jetphotos.presentation.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -7,10 +8,11 @@ import androidx.navigation.compose.composable
 
 @Composable
 fun SetupNavGraph(
-    navController: NavHostController
+    navController: NavHostController,
 ) {
-    NavHost(navController = navController, startDestination = Screen.Home.route ){
-        composable(route = Screen.Home.route){
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(route = Screen.Home.route) {
+            Log.d(" Iam comit", "SetupNavGraph: Thisi s a pringie")
         }
 
     }
